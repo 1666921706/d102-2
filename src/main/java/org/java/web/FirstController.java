cb21550125a376f8cf46099962ce7e03e04eca5a
1666921706 <1666921706@qq.com>
2019-08-03 15:19:45 +0800

    提交文档

--- src/main/java/org/java/web/FirstController.java
+++ src/main/java/org/java/web/FirstController.java
@@ -1,11 +1,20 @@
 package org.java.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @Author:严辉
  * @Date: 2019/8/3 15:08
  * @Description
  */
 
-
+@RestController
 public class FirstController {
+
+    @GetMapping("first")
+    public String first(){
+
+        return "hello world";
+    }
 }
